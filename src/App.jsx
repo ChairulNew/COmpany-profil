e886798c9466ae1787ed1fc1d529fc732692a181
ChairulNew <chairularul010805>
2025-03-23 13:34:00 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+
+import Navbar from "./Components/Navbar/Navbar";
+import NavbarBottom from "./Components/Navbar/Navbar-bottom";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <NavbarBottom />
+    </>
+  );
+}
+
+export default App;
